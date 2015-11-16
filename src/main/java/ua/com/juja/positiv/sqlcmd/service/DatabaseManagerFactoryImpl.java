d1613a1e6db973b88d72a9e83b1db3ba973bce08
POSITIV <mail.positiv@gmail.com>
2015-11-16 20:15:24 +0200

    m

--- src/main/java/ua/com/juja/positiv/sqlcmd/service/DatabaseManagerFactoryImpl.java
+++ src/main/java/ua/com/juja/positiv/sqlcmd/service/DatabaseManagerFactoryImpl.java
@@ -7,8 +7,9 @@ import ua.com.juja.positiv.sqlcmd.databasemanager.JDBCDatabaseManager;
  * Created by POSITIV on 15.11.2015.
  */
 public class DatabaseManagerFactoryImpl implements DatabaseManagerFactory {
+
     @Override
     public DatabaseManager createDatabaseManager(){
-     return new JDBCDatabaseManager();
+        return new JDBCDatabaseManager();
     }
 }
