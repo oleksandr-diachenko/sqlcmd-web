c315284565f890c4969998e4d83fd443eb3c238a
POSITIV <mail.positiv@gmail.com>
2016-01-02 15:47:47 +0200

    разбил таблицу на две и записал скрипты

--- src/main/resources/sql/v2.sql
+++ src/main/resources/sql/v2.sql
@@ -38,25 +38,6 @@ ALTER TABLE user_action_seq
   OWNER TO postgres;
 
 
--- Table: user_actions
-
--- DROP TABLE user_actions;
-
-CREATE TABLE user_actions
-(
-  id integer NOT NULL DEFAULT nextval('user_action_seq'::regclass),
-  db_name text,
-  user_name text,
-  action text,
-  CONSTRAINT user_action_id_pk PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE user_actions
-  OWNER TO postgres;
-
-
 -- Sequence: database_connection_seq
 
 -- DROP SEQUENCE database_connection_seq;
